538fc31725d7eea915b6718024a96552bc5359ea
smhrd <smhrd@DESKTOP-G7RUE98>
2021-10-01 16:07:33 +0900

    코드추가

--- GitTest/src/main/java/com/smhrd/Test.java
+++ GitTest/src/main/java/com/smhrd/Test.java
@@ -3,6 +3,8 @@ package com.smhrd;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Github Push Test!");
+		System.out.println("Add Code!");
+		System.out.println("Hello");
 	}
 
 }
