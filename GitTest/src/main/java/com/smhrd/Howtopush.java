93d306f574551d5c72931e0045ddad7767003215
smhrd <smhrd@DESKTOP-TIE5GIH>
2021-10-01 17:22:28 +0900

    푸쉬방법2

--- GitTest/src/main/java/com/smhrd/Howtopush.java
+++ GitTest/src/main/java/com/smhrd/Howtopush.java
@@ -3,8 +3,8 @@ package com.smhrd;
 public class Howtopush {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("먼저 작성하고 얘를 Staged Changes로 보내고 Commit Message 작성하고 Commit and Push");
+		System.out.println("그 다음에 뭐 누르냐면 .... ");
 	}
 
 }
