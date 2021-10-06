39461b9848ce4a3d7a7f6d8d7fe2f35c120c849d
박희선 <78364397+heeseon0201@users.noreply.github.com>
2021-10-06 11:31:04 +0900

    Delete GitTest directory
    GitTest 폴더 삭제 이제 프로젝트폴더로 넘어갑시당

--- GitTest/src/main/java/com/smhrd/hansung.java
+++
@@ -1,9 +0,0 @@
-package com.smhrd;
-
-public class hansung {
-	public void adfdasf() {
-	}
-	public void asdf() {
-		System.out.println("¿Ã∞« π∫µ•..? ø÷ ");
-	}
-}
--- GitTest/src/main/java/com/smhrd/sadfsd.java
+++
@@ -1,5 +0,0 @@
-package com.smhrd;
-
-public class sadfsd {
-	
-}
