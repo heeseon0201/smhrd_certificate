91efe34a447822b02e0ee138ac03c068aef4042a
smhrd <smhrd@DESKTOP-DJU34A8>
2021-10-12 11:44:29 +0900

    MemberVO, DAO 안올라가서 다시푸쉬

---
+++ Project/src/main/java/com/model/MemberDAO.java
@@ -0,0 +1,135 @@
+package com.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+	Connection conn = null;
+	PreparedStatement psmt = null;
+	ResultSet rs = null;
+	
+	public void getConnection() {
+		try {
+			// JDBC 드라이버 동적 로딩
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String password = "hr";
+			
+			// 데이터 베이스 연결객체 생성
+			conn = DriverManager.getConnection(url, user, password);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// DB close 메소드
+	public void close() {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (psmt != null) {
+				psmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 회원가입 메서드
+	public int Join(String member_id, String member_NM, String member_tel, String member_address,
+			String member_interest, String member_job, String member_pw) {
+		int cnt = 0;
+		
+		try {
+			getConnection();
+			
+			// 회원가입 sql문
+			String sql = "insert into Member values(Member_seq.nextval, ?, ?, ?, ?, ?, ?, ?)";
+			
+			// SQL 실행 객체 생성
+			psmt = conn.prepareStatement(sql);
+			
+			// 바인드 변수 채우기
+			psmt.setString(1, member_id);
+			psmt.setString(2, member_NM);
+			psmt.setString(3, member_tel);
+			psmt.setString(4, member_address);
+			psmt.setString(5, member_interest);
+			psmt.setString(6, member_job);
+			psmt.setString(7, member_pw);
+			
+			// sql문 실행 후 결과처리
+			cnt = psmt.executeUpdate();
+			
+			if(cnt > 0) {
+				System.out.println("회원가입성공!");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("회원가입 실패");
+		} finally {
+			close();
+		}
+		return cnt;
+	}
+	
+	// 로그인 메소드
+	public MemberVO Login(String member_id, String member_pw) {
+		int cnt = 0;
+		MemberVO vo = null;
+		
+		try {
+			getConnection();
+			
+			// 로그인 sql문
+			String sql = "select * from Member where email=? and pw=?";
+			
+			// SQL 실행 객체 생성
+			psmt = conn.prepareStatement(sql);
+			
+			// 바인드 변수 채우기
+			psmt.setString(1, member_id);
+			psmt.setString(2, member_pw);
+			
+			// sql문 실행
+			rs = psmt.executeQuery();
+			
+			// 결과처리
+			if(rs.next()) {		
+				System.out.println("로그인성공!");
+				
+				int get_no = rs.getInt("member_no");
+				String get_id = rs.getString("member_id");
+				String get_NM = rs.getString("member_NM");
+				String get_tel = rs.getString("member_tel");
+				String get_address = rs.getString("member_address");
+				String get_interest = rs.getString("member_interest");
+				String get_job = rs.getString("member_job");
+				String get_pw = rs.getString("member_pw");
+				
+				vo = new MemberVO(get_no, get_id, get_NM, get_tel, get_address, get_interest, get_job, get_pw);
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("로그인 실패");
+		} finally {
+			close();
+		}
+		return vo;
+	}
+	
+	// 회원정보관리 메소드
+	//public 
+	
+}
---
+++ Project/src/main/java/com/model/MemberVO.java
@@ -0,0 +1,93 @@
+package com.model;
+
+public class MemberVO {
+	// 필드 선언
+	// private : 현재 객체 내에서만 접근가능
+	private int member_no;	// 회원번호
+	private String member_id;	// 회원아이디
+	private String member_NM;	// 이름
+	private String member_tel;	// 전화번호
+	private String member_address;	// 주소
+	private String member_interest;	// 관심분야
+	private String member_job;	// 직업
+	private String member_pw;	// 비밀번호
+	
+	// 생성자
+	public MemberVO(int member_no, String member_id, String member_NM, String member_tel, String member_address,
+			String member_interest, String member_job, String member_pw) {
+		super();
+		this.member_no = member_no;
+		this.member_id = member_id;
+		this.member_NM = member_NM;
+		this.member_tel = member_tel;
+		this.member_address = member_address;
+		this.member_interest = member_interest;
+		this.member_job = member_job;
+		this.member_pw = member_pw;
+	}
+
+	// 메소드(get,set)
+	public int getMember_no() {
+		return member_no;
+	}
+
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getMember_NM() {
+		return member_NM;
+	}
+
+	public void setMember_NM(String member_NM) {
+		this.member_NM = member_NM;
+	}
+
+	public String getMember_tel() {
+		return member_tel;
+	}
+
+	public void setMember_tel(String member_tel) {
+		this.member_tel = member_tel;
+	}
+
+	public String getMember_address() {
+		return member_address;
+	}
+
+	public void setMember_address(String member_address) {
+		this.member_address = member_address;
+	}
+
+	public String getMember_interest() {
+		return member_interest;
+	}
+
+	public void setMember_interest(String member_interest) {
+		this.member_interest = member_interest;
+	}
+
+	public String getMember_job() {
+		return member_job;
+	}
+
+	public void setMember_job(String member_job) {
+		this.member_job = member_job;
+	}
+
+	public String getMember_pw() {
+		return member_pw;
+	}
+
+	public void setMember_pw(String member_pw) {
+		this.member_pw = member_pw;
+	}
+}
