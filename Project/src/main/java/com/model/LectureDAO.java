5e3ba7f1f8d55a7fec6647513c997508ec4b1bb2
smhrd <smhrd@DESKTOP-DJU34A8>
2021-10-15 15:59:47 +0900

    Lecture dao vo수정

--- Project/src/main/java/com/model/LectureDAO.java
+++ Project/src/main/java/com/model/LectureDAO.java
@@ -111,7 +111,6 @@ public class LectureDAO {
 				
 				// 검색 sql문
 				sql = "select lecture_name from Lecture where lecture_name like '%" + hitwords[i] + "%'";//푸시
-
 //				sql = "select * from Lecture where lecture_name like '%?%' OR lecture_teach like '%?%' OR lecture_count like '%?%' OR lecture_price like '%?%' OR lecture_point like '%?%' OR lecture_review like '%?%' OR lecture_url like '%?%' OR lecture_cat like '%?%'";
 				System.out.println("1");
 				// SQL 실행 객체 생성
